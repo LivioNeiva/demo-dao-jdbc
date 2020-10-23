804e7848718dbf494c3fa8ba69d18f8656ff9648
LivioNeiva <livio_neiva@hotmail.com>
2020-10-23 18:53:28 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entidade.Departamento;
+import model.entidade.Seller;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 		Departamento obj = new Departamento(1, "Livros");
 		
 		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
